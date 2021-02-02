14a77203ec7804010d3cadaa36a35a80a2c6cc9a
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-02-02 09:58:14 +0700

    menambahkan lokasi

--- app/src/main/java/com/kakapo/weatherapps/activity/MainActivity.kt
+++ app/src/main/java/com/kakapo/weatherapps/activity/MainActivity.kt
@@ -1,18 +1,47 @@
 package com.kakapo.weatherapps.activity
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.location.*
 import com.kakapo.weatherapps.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    private val mLocationCallback = object: LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("Current Latitude", "$latitude")
+
+            val longitude = mLastLocation.longitude
+            Log.i("Current longitude", "$longitude")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         if(!isLocationEnabled()){
             Toast.makeText(
@@ -25,11 +54,39 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }else{
-            Toast.makeText(
-                    this,
-                    "Your location Provider is Already ON.",
-                    Toast.LENGTH_SHORT
-            ).show()
+            Dexter.withActivity(this)
+                    .withPermissions(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                    .withListener(object: MultiplePermissionsListener{
+                        override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                            if(report!!.areAllPermissionsGranted()){
+                                requestLocationData()
+                            }
+
+                            if(report.isAnyPermissionPermanentlyDenied){
+                                Toast.makeText(
+                                        this@MainActivity,
+                                        "You have denied location permission. Please enable them " +
+                                                "as it is mandatory for the App to work",
+                                        Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        }
+
+                        override fun onPermissionRationaleShouldBeShown(
+                                permissions: MutableList<PermissionRequest>?,
+                                token: PermissionToken?
+                        ) {
+                            showRationalDialogForPermissions()
+                        }
+
+                    })
+                    .onSameThread()
+                    .check()
         }
     }
 
@@ -40,5 +97,37 @@ class MainActivity : AppCompatActivity() {
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this)
+                .setMessage("It looks like you have turned off permissions required " +
+                        "for this feature. it an be enabled under settings")
+                .setPositiveButton("GO TO SETTINGS"){ _, _ ->
+                    try{
+
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    }catch(e: Exception){
+                        e.printStackTrace()
+                    }
+                }
+                .setNegativeButton("Cancel"){dialog, _->
+                    dialog.dismiss()
+                }.show()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData(){
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback,
+                Looper.myLooper()
+        )
     }
 }
